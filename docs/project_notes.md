460a77888eb69818b0923123dc83352a9fd71ca0
syanco <syanco@gmail.com>
2022-01-07 13:21:15 -0500

    cont wf, cleaning and processing, prpj docs

--- docs/project_notes.md
+++ docs/project_notes.md
@@ -1,11 +1,24 @@
-## TODO
-
+## TODO  
+* Export conda env .yml files
 
 ## Major Activity Log
 
 |Date|Activity|
 |:-|:------------|
-|2022-01-01|Initialize repo, docs, etc.|
+|2022-01-05|Initialize repo, docs, etc.|
+|2022-01-07|Start building top-level workflow and cleaning scripts|
 
-## Notes
-* Proposed a diretcory structure in README.md - not positive this is what we want yet... 
+
+## Notes  
+
+### General
+
+* Proposed a directory structure in README.md - not positive this is what we want yet... 
+
+
+### Building DB
+
+
+### Process and clean data
+
+* using a control file approach for the study period segmentation/annotation b/c this allows future flexibility to assign individual-/cohort-specific study periods according to some covariate (e.g., taxa, location, etc.)
---
+++ src/workflow.sh
@@ -0,0 +1,52 @@
+                  ##########################################                    
+                  ####                                  ####
+                  ####    COVID-19 & Animal Movement    ####
+                  ####          Workflow Script         ####
+                  ####                                  ####
+                  ####              contact:            ####
+                  ####       scott.yanco@yale.edu       ####
+                  ####                                  ####
+                  ##########################################
+                  
+#------------------------------------------------------------------------------#
+
+# Top-level workfow script to execute/describe steps to run analyses associated 
+# with the North American COVID-19 & Animal Movemnt project.  Some steps are 
+# only described (i.e., do not contain code) due to restrictions on data, access
+# credentials, non-coded steps, etc.
+
+# CONDA ENVIRONMENTS
+# 
+# Much of this code is meant to be executed within coda environments. The 
+# directory /analysis/src/conda_envs contains .yml files from which the 
+# requisite conda environments can be built.
+
+#------------------------------------------------------------------------------#
+
+
+####----  Initialization  ----####
+
+# Define working directory - should point to project root folder
+wd=~/projects/covid-19_movement
+
+
+####----  Build Initial Database  ----####
+
+# Go to working directory
+cd $wd
+
+# Build a mosey_db-style SQLite database from movebank to store raw data
+# See https://github.com/benscarlson/mosey_db for more detail.
+
+# TODO:  @diego & @ben, add 'final' code to build db here
+
+
+####----  Prep and Clean Data ----####
+
+# Make a copy of db to be modified
+mv $wd/raw_data/mosey.db $wd/processed_data/mosey_mod.db
+
+# Process and clean data:
+#   * add study period annotation
+#   * remove events outside study area
+#   * basic data cleaning
