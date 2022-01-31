55e9cae489517e728eefc0ac8681a5a6fa183443
Scott Yanco <sy522@grace2.grace.hpc.yale.internal>
2022-01-31 13:22:49 -0500

    submit sh

--- src/hpc/submit_dBBMM.sh
+++ src/hpc/submit_dBBMM.sh
@@ -4,9 +4,9 @@
 #SBATCH --mail-type ALL
 #SBATCH --mail-user scott.yanco@yale.edu
 #SBATCH --partition pi_jetz
-#SBATCH -c 36
-#SBATCH --mem-per-cpu 20G
-#SBATCH -J fit_dBBMMs_2022-01-20
+#SBATCH -c 24
+#SBATCH --mem-per-cpu 10G
+#SBATCH -J fit_dBBMMs_2022-01-26
 
 # Load conda env
 module load miniconda
