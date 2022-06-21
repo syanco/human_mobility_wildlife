93fe73368ee89a4423252508eb1674f1c5c7f760
syanco <syanco@gmail.com>
2022-06-21 15:04:21 -0400

    updating workflow, starting integrating RO code, new branch for wf unification, filename changes, directory updates

--- src/hpc/run_clean_movement.sh
+++ src/hpc/run_clean_movement.sh
@@ -5,7 +5,6 @@
 #SBATCH --mail-user scott.yanco@yale.edu
 #SBATCH -c 1
 #SBATCH --mem-per-cpu 50G
-#SBATCH -J prep_covid_2022-01-20
 
 # Load conda env
 module load miniconda
@@ -15,4 +14,4 @@ conda activate spatial
 wd=~/project/covid-19_movement
 
 # Execute cleaning script (non parallel)
-Rscript $wd/analysis/src/01-prep_and_clean.r --db $wd/processed_data/mosey_mod.db
+Rscript $wd/analysis/src/workflow/clean_movement.r --db $wd/processed_data/mosey_mod.db
--- src/workflow.sh
+++ src/workflow.sh
@@ -3,7 +3,7 @@
                   ####    COVID-19 & Animal Movement    ####
                   ####          Workflow Script         ####
                   ####                                  ####
-                  ####              contact:            ####
+                  ####             contact:             ####
                   ####       scott.yanco@yale.edu       ####
                   ####                                  ####
                   ##########################################
@@ -11,61 +11,118 @@
 #------------------------------------------------------------------------------#
 
 # Top-level workfow script to execute/describe steps to run analyses associated 
-# with the North American COVID-19 & Animal Movemnt project.  Some steps are 
-# only described (i.e., do not contain code) due to restrictions on data, access
-# credentials, non-coded steps, etc.
+# with the North American COVID-19 & Animal Movemnt project. 
+
+# This project is led by: Ruth Oliver, Diego Ellis-Soto, Scott Yanco, Brett 
+# Jesmer, & Walter Jetz (PI). 
+
+# This workflow is generally built to be run in a high performance computing 
+# (HPC) environment. Thus, most steps in the workflow simply use `sbatch` to 
+# submit a job to the slurm manager.  However, we also include (commented) code 
+# to run each step without submission to a slurm manager if desired 
+# (not recommended).
+
+# This workflow also assumes a specific directory strcuture.  See README for 
+# detail son directory strcuture.
 
 # CONDA ENVIRONMENTS
 # 
 # Much of this code is meant to be executed within coda environments. The 
 # directory /analysis/src/conda_envs contains .yml files from which the 
-# requisite conda environments can be built.
+# requisite conda environments can be built. See readme for details on building 
+# conda environments
 
 #------------------------------------------------------------------------------#
 
 
 ####----  Initialization  ----####
 
-# Define working directory - should point to project root folder
+# define working directory
 wd=~/project/covid-19_movement
-
-
-####----  Build Initial Database  ----####
+src=$wd/analysis/src
 
 # Go to working directory
 cd $wd
 
-# Build a mosey_db-style SQLite database from movebank to store raw data
-# See https://github.com/benscarlson/mosey_db for more detail.
+# Make scripts executable (mostly to make docopts visible via `-h` flag)
+chmod +x $src/clean_movement.r
 
-# TODO:  @diego & @ben, add 'final' code to build db here
+# Turn on miniconda (only necessary on HPC when not using SLURM)
+# module load miniconda
+
 
 
 ####----  Prep and Clean Data ----####
 
-# Make a copy of db to be modified
-cp $wd/raw_data/mosey.db $wd/processed_data/mosey_mod.db
+  ##-- Working DB --##
 
-#activate spatial env
-# module load miniconda
-# conda activate spatial
+    # Make a copy of db to be modified
+    cp $wd/raw_data/mosey.db $wd/processed_data/mosey_mod.db
+  ##
 
-# Process and clean data:
-#   * add study period annotation
-#   * remove events outside study area
-#   * basic data cleaning
+  ##-- Run Cleaning --##
+  
+    # Inputs:   db:event + analysis/ctfs/dates.csv
+    # Outputs:  db:event_mod + db:event_clean
+    # Depends:  clone of mosey_env (https://github.com/benscarlson/mosey_env)
+  
+    # SLURM:
+    sbatch $src/hpc/run_clean_movement.sh
 
-# CHOOSE ONE:
-  # Run on HPC:
-  sbatch analysis/src/hpc/submit_prep.sh
+    # ON DEMAND:
+    # conda activate spatial
+    # Rscript $src/workflow/clean_movement.r --db $wd/processed_data/mosey_mod.db
+  
+    # TODO: 
+    #   * Remove hardcoded study filters
+    #   * Simplify date filtering
+    #   * Add spatial filtering (events outside US).
+    #   * Allow quantile thresholds to be passed by arg
+  ##
 
-  # Run Local:
-  # Rscript $wd/analysis/src/01-prep_and_clean.r --db $wd/processed_data/mosey_mod.db
+####
+
 
 
 ####----  Annotate Data ----####
 
+  ##-- Environmental Annotations --#
+  
+    # Inputs: db:event_clean + analysis/ctfs/env.csv
+    # Outputs: db:event_clean (annotated)
+    
+    # INTERACTIVE (script must be run interactively)
+    $src/wf-mosey_env.sh
+  ##
+    
+  ##-- Census Annotations --##
+  
+    #- Intersect events with cbg geometries -#
+    
+      # Inputs:   db:event_clean + cbg shp file
+      # Outputs:  csv per job (event_id + cbg info)
 
+      # DSQ: (Yale-specific)
+      # module load R/4.1.0-foss-2020b
+      # Rscript $src/workflow/create_intersection_joblist.R
+
+      #module load dSQ
+      #dsq --job-file $src/workflow/joblist.txt --mem-per-cpu 40g -t 2- 
+
+      # UPDATE WITH DATE
+      #sbatch dsq-joblist-2022-05-12.sh
+      
+      # TODO: 
+      #   * Missing code for last step above
+      #   * Rewrite for non-DSQ (for reproducibility)?
+    #
+    
+    #-
+  
+  
+  ##
+
+####
 
 
 ####----  Analysis ----####
