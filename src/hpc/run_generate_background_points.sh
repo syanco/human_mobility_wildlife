34e2b149693b17502707dc68692389fe0bb916c2
syanco <syanco@gmail.com>
2022-06-29 15:23:48 -0400

    fix partition

--- src/hpc/run_generate_background_points.sh
+++ src/hpc/run_generate_background_points.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=ruth.oliver@yale.edu
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=1000g -t 2-
-#SBATCH --partition=general,bigmem,pi_jetz
+#SBATCH --partition=pi_jetz
 #SBATCH -C avx2
 
 module load miniconda
