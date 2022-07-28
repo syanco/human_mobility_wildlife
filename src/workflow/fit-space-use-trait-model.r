6404a89a1f8053024d2a1bc4d5669ed7efa857cd
syanco <syanco@gmail.com>
2022-07-28 10:57:06 -0400

    fix formula

--- src/workflow/fit-space-use-trait-model.r
+++ src/workflow/fit-space-use-trait-model.r
@@ -132,7 +132,7 @@ dat <- size %>%
 # ==== Perform analysis ====
 
 #declare model form
-form <-  bf(log_area_scale ~ sg_norm*ghm_scale + ndvi_scale + lst_scale + (1 |species/grp) + ar(time = wk, gr = grp))
+form <-  bf(log_area_scale ~ sg_norm*ghm_scale*diet + ndvi_scale + lst_scale + (1 |species/grp) + ar(time = wk, gr = grp))
 message("Fitting models with formula:")
 print(form)
 
