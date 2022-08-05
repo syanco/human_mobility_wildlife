55103447ee7d8c281dda563565d1a6e875ca144d
syanco <syanco@gmail.com>
2022-08-04 21:23:50 -0400

    space use ghm model

--- src/hpc/run_fit_space_use_sg_models.sh
+++ src/hpc/run_fit_space_use_sg_models.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition pi_jetz
 #SBATCH -c 24
 #SBATCH --mem-per-cpu 10G
-#SBATCH -J fit_space_use_sg_mods
+#SBATCH -J fit_space_use_ghm_mods
 
 # Load conda env
 module load miniconda
@@ -18,4 +18,4 @@ wd=/gpfs/loomis/project/jetz/sy522/covid-19_movement
 cd $wd
 
 # Execute model script 
-Rscript $wd/analysis/src/workflow/fit-space-use-sg-models.r $wd/out/dbbmm_size.csv $wd/out/single_species_models/area_sg 24 10 10000 5
+Rscript $wd/analysis/src/workflow/fit-space-use-ghm-models.r $wd/out/dbbmm_size.csv $wd/out/single_species_models/area_ghm 24 10 10000 5
