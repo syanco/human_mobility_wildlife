25848318a183289bbc2248d6aea23f00d32075bb
syanco <syanco@gmail.com>
2022-08-10 14:55:55 -0400

    fix dsq gen

--- src/workflow/create_bg_annotation_joblist_moose.r
+++ src/workflow/create_bg_annotation_joblist_moose.r
@@ -34,7 +34,7 @@ files <- list.files(paste0(.wd,'/out/ssf-background-pts/moose'),full.names = TRU
 n_total <- length(files)
 
 
-n <- 64
+n <- 48
 n_events <- round(n_total/n)
 # n_events <- ifelse((n_total/n)==n_events, n_events, n_events)
 
@@ -42,7 +42,7 @@ start_ix <- seq(from = 1, to = n_total, by = n_events)
 end_ix <- c(seq(from = n_events, to = n_total, by = n_events))
 
 
-joblist <- data.frame("string" = rep(paste0(" module load miniconda; conda activate spatial; Rscript ",.datPF,"annotate-background-sg-ghm2.r "), times = n),
+joblist <- data.frame("string" = rep(paste0(" module load miniconda; conda activate spatial; Rscript ",.datPF,"annotate-background-sg-ghm2.r "), times = n_events),
                       "arg1" = start_ix,
                       "arg2" = end_ix,
                       "arg3" = seq(from = 1, to = n, by = 1))
