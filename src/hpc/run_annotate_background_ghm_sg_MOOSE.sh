dbae10b73cfb9723af7fc65a4562e0b3c55fab8c
syanco <syanco@gmail.com>
2022-08-11 10:50:02 -0400

    adding slurm script

---
+++ src/hpc/run_annotate_background_ghm_sg_MOOSE.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=ghm_annotation-moose-BG
+#SBATCH --cpus-per-task=24
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=scott.yanco@yale.edu
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=100g -t 3:00:00
+#SBATCH --partition=day
+
+# module load R
+module load miniconda
+conda activate spatial
+
+# Declare WD
+wd=/gpfs/loomis/pi/jetz/sy522/covid-19_movement
+src=$wd/analysis/src/workflow
+
+# Move to WD
+cd $wd
+
+Rscript $src/annotate-background-sg-ghm-MOOSE_scrath.r 24
