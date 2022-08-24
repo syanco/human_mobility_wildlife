ef923f0ae1ade645d4b1a9eb6855bbb1ebee0853
syanco <syanco@gmail.com>
2022-08-24 13:39:26 -0400

    expand ghm sg anno to all animals

---
+++ src/hpc/run_annotate_background_ghm_sg_ALL.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=ghm_annotation-moose-BG
+#SBATCH --cpus-per-task=30
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=scott.yanco@yale.edu
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=50g -t 1-
+#SBATCH --partition=bigmem
+
+# module load R
+module load miniconda
+conda activate spatial
+
+# Declare WD
+wd=/gpfs/loomis/pi/jetz/sy522/covid-19_movement
+src=$wd/analysis/src/workflow
+
+# Move to WD
+cd $wd
+
+Rscript $src/annotate-background-sg-ghm-ALL.r 30
