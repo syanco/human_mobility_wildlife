392284c450c14da71995367b1ca0eaa28337c04c
syanco <syanco@gmail.com>
2022-08-26 09:22:16 -0400

    waaltim

--- src/hpc/run_annotate_background_ghm_sg_ALL.sh
+++ src/hpc/run_annotate_background_ghm_sg_ALL.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=scott.yanco@yale.edu
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=50g -t 3-
+#SBATCH --mem-per-cpu=50g -t 2-
 #SBATCH --partition=bigmem
 
 # module load R
