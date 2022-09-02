6c8609a27a1ec4bb1505d5e07d6e4cb3bbd41ec7
syanco <syanco@gmail.com>
2022-09-02 13:54:44 -0400

    fix write out

--- src/hpc/run_fit_niche_breadth_additive_models.sh
+++ src/hpc/run_fit_niche_breadth_additive_models.sh
@@ -18,4 +18,4 @@ wd=/gpfs/loomis/pi/jetz/sy522/covid-19_movement
 cd $wd
 
 # Execute model script 
-Rscript $wd/analysis/src/workflow/fit-niche-breadth-additive-models.r $wd/out/niche_determinant_anthropause.csv $wd/out/dbbmm_size.csv $wd/out/single_species_models/niche_additive 24 10 10000 5
+Rscript $wd/analysis/src/workflow/fit-niche-breadth-additive-models.r $wd/out/niche_determinant_anthropause.csv $wd/out/dbbmm_size.csv $wd/out/single_species_models/niche_controlled 24 10 10000 5
