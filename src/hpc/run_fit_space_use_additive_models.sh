f1d90d3d03da8079e292759cf9b9988cdc1e3b39
syanco <syanco@gmail.com>
2022-09-02 13:54:09 -0400

    niceh model controlling for space use, adding partitions

--- src/hpc/run_fit_space_use_additive_models.sh
+++ src/hpc/run_fit_space_use_additive_models.sh
@@ -3,7 +3,7 @@
 #SBATCH -t 1-
 #SBATCH --mail-type ALL
 #SBATCH --mail-user scott.yanco@yale.edu
-#SBATCH --partition pi_jetz
+#SBATCH --partition pi_jetz, day
 #SBATCH -c 24
 #SBATCH --mem-per-cpu 10G
 #SBATCH -J fit_additive_space_use_mods
