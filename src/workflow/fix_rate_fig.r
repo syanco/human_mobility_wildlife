cc45a3a304b3591b5e52439a31d868e4f0cef7e7
julietcohen <jscohen@bren.ucsb.edu>
2025-05-19 10:44:24 -0700

    fix rate script to calc min and max per species

---
+++ src/workflow/fix_rate_fig.r
@@ -0,0 +1,186 @@
+#!/usr/bin/env Rscript 
+#
+# This script generates fix rate for each ind/year combo, which can be used to
+# determine the fix rater per study for supplementary figure 
+
+# ==== Setup ====
+
+'
+Estimate fix rates for pre-segemented 
+migratory periods across multiple individuals
+
+Usage:
+fix_rate_fig.r <db> <out> <nc>
+fix_rate_fig.r (-h | --help)
+
+Parameters:
+  db: path to movement database. 
+  out: path to output directory.
+  nc: number of cores for parallel processing
+  
+Options:
+-h --help     Show this screen.
+-v --version     Show version.
+' -> doc
+
+#---- Input Parameters ----#
+if(interactive()) {
+  library(here)
+  
+  .wd <- getwd()
+  rd <- here::here
+  
+  .outPF <- file.path(.wd,'out')
+  .dbPF <- file.path('')
+  
+  .nc <- 2
+  
+} else {
+  library(docopt)
+  library(rprojroot)
+  
+  ag <- docopt(doc, version = '0.1\n')
+  .wd <- getwd()
+  
+  source(file.path(.wd, 'src/funs/input_parse.r'))
+  
+  .dbPF <- makePath(ag$db)
+  .outPF <- makePath(ag$out)
+  .nc <- ag$nc
+  
+}
+
+#---- Initialize Environment ----#
+# Start time
+t0 <- Sys.time()
+
+# Run startup
+source(file.path(.wd,'src/startup.r'))
+
+# Load packages
+suppressWarnings(
+  suppressPackageStartupMessages({
+    library(DBI)
+    library(RSQLite)
+    library(lubridate)
+    library(move)
+    library(doMC)
+    library(foreach)
+    library(glue)
+  }))
+
+#Source all files in the auto load funs directory
+list.files(file.path(.wd,'src/funs/auto'),full.names=TRUE) %>%
+  walk(source)
+
+#---- Initialize database ----#
+message("Initializing database connection...")
+
+invisible(assert_that(file.exists(.dbPF)))
+db <- dbConnect(RSQLite::SQLite(), .dbPF, `synchronous` = NULL)
+invisible(assert_that(length(dbListTables(db))>0))
+indtb <- tbl(db,'individual_clean') %>%
+# note that the following species name fixes were also introduced to
+# trimming step, so can remove here when workflow is restarted
+  mutate(taxon_canonical_name = case_when(
+        study_id == 2548691779 ~ "Odocoileus hemionus",
+        study_id == 2575515057 ~ "Cervus elaphus",
+        study_id == 1044238185 ~ "Alces alces",
+        TRUE ~ taxon_canonical_name)) %>%
+  collect() 
+
+indtb <- indtb[!duplicated(indtb),]
+
+message("Disconnecting from database...")
+dbDisconnect(db)
+
+ind <- indtb %>% 
+  pull(individual_id)
+
+#remove duplicated created due to merging of dbs
+ind <- ind[!duplicated(ind)] 
+
+yearvec <- c("2019", "2020")
+
+registerDoMC(.nc)
+
+message("Starting fix rate calcs")
+
+foreach(j = 1:length(ind), .errorhandling = "pass", .inorder = F) %:%
+  foreach(i = 1:2, .errorhandling = "pass", .inorder = F) %dopar% {
+
+    message(glue("Starting individual {ind[j]}, year {yearvec[i]}..."))  
+    
+    invisible(assert_that(file.exists(.dbPF)))
+    db <- dbConnect(RSQLite::SQLite(), .dbPF, `synchronous` = NULL)
+    invisible(assert_that(length(dbListTables(db))>0))
+    
+    evt0 <- tbl(db, "event_clean")
+    
+    scientificname <- indtb %>% 
+      filter(individual_id == !!ind[j]) %>% 
+      pull(taxon_canonical_name)
+    
+    studyid <- indtb %>% 
+      filter(individual_id == !!ind[j]) %>% 
+      pull(study_id)
+    
+    evt_mod <- evt0 %>% 
+      filter(individual_id == !!ind[j]) %>% 
+      filter(yr == !!yearvec[i]) %>%
+      collect() %>% 
+      mutate(timestamp = as.POSIXct(timestamp, format = "%Y-%m-%d %T")) %>% 
+      distinct() %>% 
+      # sort by timestamp
+      arrange(timestamp)
+    
+    dbDisconnect(db)
+    
+    # check that the filtered dataframe has any data
+    if (nrow(evt_mod) == 0) {
+
+      message(glue("No paired data available for ind {ind[j]}, yr {yearvec[i]}; no fix rate available."))
+      
+      # move onto the next iteration of ind and year
+      return(NULL)
+      
+    } else {
+
+      tryCatch({
+        # make minimal df for `move`    
+        evt_tmp <- evt_mod %>% 
+          select(lon, lat, timestamp, wk)
+        
+        evt_mv <- move(x=evt_tmp$lon, y=evt_tmp$lat, time=evt_tmp$timestamp,
+                        proj=CRS("+proj=longlat"))
+
+        #id "intended fix rate"
+        fix_min <- min(timeLag(x=evt_mv, units="mins"))
+        fix_max <- max(timeLag(x=evt_mv, units="mins"))
+
+        fix_df <- data.frame("study_id" = studyid, 
+                              "species" = scientificname,
+                              "individual_id" = ind[j],
+                              "year" = yearvec[i],
+                              "fix_rate_min" = fix_min,
+                              "fix_rate_max" = fix_max)
+
+        write.table(fix_df, 
+                glue("{.outPF}/fixrate_min_max.csv"), 
+                append = T, 
+                row.names = F, 
+                col.names = F, 
+                sep = ",")
+      
+      }, error = function(e){
+        
+        # catch the exact error reported from the move package
+        error_msg <- conditionMessage(e)
+        # log info for failure with error message
+        message(glue("ERROR in estimating or saving fix rate for {scientificname} ind {ind[j]}, yr {yearvec[i]}:\n{error_msg}", 
+                                      "\n"))
+      })
+    }
+  }
+
+message(glue('Script complete in {diffmin(t0)} minutes'))
